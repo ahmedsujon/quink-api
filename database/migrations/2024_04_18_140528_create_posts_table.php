803632dcfa4137f4bd8861698be27abaf78404bd
nz-hridoy <nayeemuzaman05@gmail.com>
2024-04-18 20:15:18 +0600

    posts table

---
+++ database/migrations/2024_04_18_140528_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->text('caption')->nullable();
+            $table->longText('content')->nullable();
+            $table->longText('tags')->nullable();
+            $table->enum('type', ['photo', 'video', 'text'])->nullable();
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
