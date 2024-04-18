9c69e00e8035b23e7891038c6a95edb240952b57
nz-hridoy <nayeemuzaman05@gmail.com>
2024-04-18 23:30:38 +0600

    chat + message table

---
+++ database/migrations/2024_04_18_172457_create_chats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from')->nullable();
+            $table->unsignedBigInteger('to')->nullable();
+            $table->text('last_msg')->nullable();
+            $table->tinyInteger('notification_status')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
