e19ea45c4209d35a8629fd8502a7a6df1edea8d9
ahmedsujon <sujonahmed424@gmail.com>
2024-08-14 11:45:08 +0600

    logo updated

--- resources/views/livewire/admin/layouts/inc/header.blade.php
+++ resources/views/livewire/admin/layouts/inc/header.blade.php
@@ -6,19 +6,18 @@
                 <div class="navbar-brand-box">
                     <a href="{{ route('admin.dashboard') }}" class="logo logo-dark">
                         <span class="logo-sm">
-                            <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                            <img src="{{ asset('assets/images/quink-sm.png') }}" alt="" height="22">
                         </span>
                         <span class="logo-lg">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="" height="17">
+                            <img src="{{ asset('assets/images/quink.png') }}" alt="" height="17">
                         </span>
                     </a>
-
                     <a href="{{ route('admin.dashboard') }}" class="logo logo-light">
                         <span class="logo-sm">
-                            <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                            <img src="{{ asset('assets/images/quink-sm.png') }}" alt="" height="22">
                         </span>
                         <span class="logo-lg">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="" height="35">
+                            <img src="{{ asset('assets/images/quink.png') }}" alt="" height="35">
                         </span>
                     </a>
                 </div>
--- resources/views/livewire/admin/layouts/inc/sidebar.blade.php
+++ resources/views/livewire/admin/layouts/inc/sidebar.blade.php
@@ -1,8 +1,6 @@
 <div>
     <div class="vertical-menu">
-
         <div data-simplebar class="h-100">
-
             <!--- Sidemenu -->
             <div id="sidebar-menu">
                 <!-- Left Menu Start -->
